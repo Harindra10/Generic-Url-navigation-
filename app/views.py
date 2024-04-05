a15f3772c310519dfffbf031eb62ed8426dae7c7
Harindra10 <harindravakadanitrkl@gmail.com>
2024-04-05 20:46:30 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def priya(request):
+    return render(request,'priya.html')
+
+def vinay(request):
+    return render(request,'vinay.html')
